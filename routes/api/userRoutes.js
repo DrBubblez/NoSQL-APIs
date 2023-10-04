3c42da12eab0d7099d5f2143fdc7d1e1047db528
Harry <harrisonminer59@gmail.com>
2023-10-04 13:58:55 -0400

    Established User API routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+// GET all users
+router.get('/users', getAllUsers);
+
+// GET a single user by its _id
+router.get('/users/:id', getUserById);
+
+// Create a new user
+router.post('/users', createUser);
+
+// Update a user by its _id
+router.put('/users/:id', updateUser);
+
+// Remove a user by its _id
+router.delete('/users/:id', deleteUser);
+
+// Add a friend to a user's friend list
+router.post('/users/:userId/friends/:friendId', addFriend);
+
+// Remove a friend from a user's friend list
+router.delete('/users/:userId/friends/:friendId', removeFriend);
+
+module.exports = router;
